91104641d7b81a1e42a8e67777bd5ec3642b958f
byynine <nutitota@tutanota.com>
2026-02-10 17:24:38 +0400

    create data dir if it doesn't exist

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <errno.h>
 #include <string.h>
+#include <sys/stat.h>
 
 // TODO: CHANGE all constants into macros
 
-// TODO: (in further versions) change to /home/user/.local/share/mimi/mimi.json
-const char *mimi_data_filepath = "tmp/data";
+// TODO: (in further versions) change to /home/user/.local/share/mimi/data
+// const char *mimi_data_filepath = "tmp/data";
+const char *mimi_data_filepath = "/home/nine/.local/share/mimi/data";
+const char *mimi_data_dirpath = "/home/nine/.local/share/mimi";
 
 // command keywords for when calling from shell
 const char *kw_remind = "remind"; // create a reminder
@@ -42,7 +46,7 @@ int main(int argc, char *argv[])
         while (fgets(line_buf, sizeof(line_buf), mimi_data))
         {
             char desc[64];
-            if (sscanf(line_buf, reminder_format_in, &desc) == 1) printf("%s\n", desc);
+            if (sscanf(line_buf, reminder_format_in, &desc) == 1) printf("%s", desc);
         }
 
         return 0;
@@ -50,12 +54,23 @@ int main(int argc, char *argv[])
 
     if (argc > 3) return 2;
 
+    // TODO: isolate into function
+    // TODO: add a check for hitting max amount of characters (bigger than buffer)
     // this appends a reminder object to data
     if (!strcmp(argv[1], kw_remind))
     {
         if (argc != 3) return 3;
+
+        if (strlen(argv[2]) > 62)
+        {
+            printf("description too big\n");
+            return 1;
+        }
+
         reminder rmn;
         snprintf(rmn.desc, sizeof(rmn.desc), "%s", argv[2]);
+
+        if (mkdir(mimi_data_dirpath, 0755) == -1) if (errno != EEXIST) return -1;
 
         FILE* mimi_data = fopen(mimi_data_filepath, "a");
         if (!mimi_data) return 4;
@@ -63,6 +78,7 @@ int main(int argc, char *argv[])
         fprintf(mimi_data, reminder_format_out, rmn.desc);
     }
     // this prints the reminders in data
+    // TODO: isolate into function
     else if (!strcmp(argv[1], kw_list))
     {
         FILE* mimi_data = fopen(mimi_data_filepath, "r");
@@ -76,7 +92,7 @@ int main(int argc, char *argv[])
         while (fgets(line_buf, sizeof(line_buf), mimi_data))
         {
             char desc[64];
-            if (sscanf(line_buf, reminder_format_in, &desc) == 1) printf("%s\n", desc);
+            if (sscanf(line_buf, reminder_format_in, &desc) == 1) printf("%s", desc);
         }
     }
 

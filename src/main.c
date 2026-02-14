363a8eb3e700d30bc59cdc3c7070d2b89b9c2c0b
byynine <nutitota@tutanota.com>
2026-02-14 11:17:23 +0400

    uppercase macros

--- src/main.c
+++ src/main.c
@@ -6,16 +6,16 @@
 // TODO: ADD indices to reminders and add remove option
 
 // filepaths for storing reminder data
-#define mimi_data_filepath "/home/nine/.local/share/mimi/data"
-#define mimi_data_dirpath "/home/nine/.local/share/mimi"
+#define MIMI_DATA_FILEPATH "/home/nine/.local/share/mimi/data"
+#define MIMI_DATA_DIRPATH "/home/nine/.local/share/mimi"
 
 // command keywords for when calling from shell
-#define kw_remind "remind"
-#define kw_list "list"
+#define CMD_REMIND "remind"
+#define CMD_LIST "list"
 
 // serialization i/o formats
-#define reminder_format_in "\"%63[^\"]\"\n"
-#define reminder_format_out "\"%s\"\n"
+#define REMINDER_FORMAT_IN "\"%63[^\"]\"\n"
+#define REMINDER_FORMAT_OUT "\"%s\"\n"
 
 typedef struct reminder
 {
@@ -27,8 +27,8 @@ int main(int argc, char *argv[])
     // display reminders when called and return
     if (argc == 1)
     {
-        FILE* mimi_data = fopen(mimi_data_filepath, "r");
-        if (!mimi_data) { printf("error: couldn't read file %s\n", mimi_data_filepath); return 1; }
+        FILE* mimi_data = fopen(MIMI_DATA_FILEPATH, "r");
+        if (!mimi_data) { printf("error: couldn't read file %s\n", MIMI_DATA_FILEPATH); return 1; }
 
         reminder rmn;
         snprintf(rmn.desc, sizeof(rmn.desc), "%s", argv[2]);
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
         while (fgets(line_buf, sizeof(line_buf), mimi_data))
         {
             char desc[64];
-            if (sscanf(line_buf, reminder_format_in, desc) == 1) printf("%s\n", desc);
+            if (sscanf(line_buf, REMINDER_FORMAT_IN, desc) == 1) printf("%s\n", desc);
         }
 
         return 0;
@@ -47,9 +47,9 @@ int main(int argc, char *argv[])
 
     // TODO: isolate into function
     // this appends a reminder object to data
-    if (!strcmp(argv[1], kw_remind))
+    if (!strcmp(argv[1], CMD_REMIND))
     {
-        if (argc != 3) { printf("error: command %s expects one more argument\n", kw_remind); }
+        if (argc != 3) { printf("error: command %s expects one more argument\n", CMD_REMIND); }
 
         reminder rmn;
 
@@ -61,24 +61,24 @@ int main(int argc, char *argv[])
 
         snprintf(rmn.desc, sizeof(rmn.desc), "%s", argv[2]);
 
-        if (mkdir(mimi_data_dirpath, 0755) == -1)
+        if (mkdir(MIMI_DATA_DIRPATH, 0755) == -1)
             if (errno != EEXIST)
             {
-                printf("error: failure when creating directory %s", mimi_data_dirpath);
+                printf("error: failure when creating directory %s", MIMI_DATA_DIRPATH);
                 return 1;
             }
 
-        FILE* mimi_data = fopen(mimi_data_filepath, "a");
-        if (!mimi_data) { printf("error: couldn't append to file %s\n", mimi_data_filepath); return 1; }
+        FILE* mimi_data = fopen(MIMI_DATA_FILEPATH, "a");
+        if (!mimi_data) { printf("error: couldn't append to file %s\n", MIMI_DATA_FILEPATH); return 1; }
 
-        fprintf(mimi_data, reminder_format_out, rmn.desc);
+        fprintf(mimi_data, REMINDER_FORMAT_OUT, rmn.desc);
     }
     // this prints the reminders in data
     // TODO: isolate into function
-    else if (!strcmp(argv[1], kw_list))
+    else if (!strcmp(argv[1], CMD_LIST))
     {
-        FILE* mimi_data = fopen(mimi_data_filepath, "r");
-        if (!mimi_data) { printf("error: couldn't read file %s", mimi_data_filepath); return 1; }
+        FILE* mimi_data = fopen(MIMI_DATA_FILEPATH, "r");
+        if (!mimi_data) { printf("error: couldn't read file %s", MIMI_DATA_FILEPATH); return 1; }
 
         reminder rmn;
         snprintf(rmn.desc, sizeof(rmn.desc), "%s", argv[2]);
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
         while (fgets(line_buf, sizeof(line_buf), mimi_data))
         {
             char desc[64];
-            if (sscanf(line_buf, reminder_format_in, desc) == 1) printf("%s\n", desc);
+            if (sscanf(line_buf, REMINDER_FORMAT_IN, desc) == 1) printf("%s\n", desc);
         }
     }
     else

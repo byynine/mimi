8008bf7809c85805085f76a764c09f9041f58e09
byynine <nutitota@tutanota.com>
2026-02-15 20:50:55 +0400

    0.5 implement reminders

--- src/main.c
+++ src/main.c
@@ -23,46 +23,50 @@
 #define DATA_FORMAT_OUT "\"%s\" %ld\n"
 
 typedef struct reminder {
-    char desc[64];       // Description of the reminder.
-    time_t rtime; // Date when the reminder will run.
+    char desc[64]; // Description of the reminder.
+    time_t rtime;  // Date when the reminder will run.
 } reminder;
 
 int main(int argc, char *argv[])
 {
-    // Display reminders when called and return.
+    // Display any reminders if its date has been met and return.
     if (argc == 1)
     {
         FILE* mimi_data = fopen(DATA_FILEPATH, "r");
         if (!mimi_data) { printf("error: couldn't read file %s\n", DATA_FILEPATH); return 1; }
 
+        time_t ctime = time(NULL);
+
+        // Print reminders.
         char line_buf[128];
-        
         while (fgets(line_buf, sizeof(line_buf), mimi_data))
         {
             char desc[64];
             time_t rtime;
             if (sscanf(line_buf, DATA_FORMAT_IN, desc, &rtime) == 2)
-            {
-                printf("%s\t%ld\n", desc, (long)rtime);
-            }
+                if (ctime > rtime)
+                    printf("reminder\t%s\n", desc);
         }
 
         fclose(mimi_data);
         return 0;
     }
 
-    // This appends a reminder object to data.
+    // Appends a reminder object to data.
     if (!strcmp(argv[1], CMD_REMIND0) || !strcmp(argv[1], CMD_REMIND1))
     {
         if (argc != 4) { printf("error: invalid argument count\n"); return 1; }
 
+        // Get current data in seconds and then add specified seconds from argument.
         char *end;
         long in_rtime = strtol(argv[3], &end, 10);
-        time_t current_time = time(NULL);
-        time_t reminder_time = current_time + in_rtime;
-
-        printf("ctime: %ld\n", (long)current_time);
-        printf("rtime: %ld\n", (long)reminder_time);
+        if (*end != '\0')
+        {
+            printf("error: command %s argument is not a valid number\n", CMD_REMIND0);
+            return 1;
+        }
+        time_t ctime = time(NULL);
+        time_t reminder_time = ctime + in_rtime;
 
         reminder rmn;
 
@@ -72,9 +76,11 @@ int main(int argc, char *argv[])
             return 1;
         }
 
+        // Add data to reminder structure.
         snprintf(rmn.desc, sizeof(rmn.desc), "%s", argv[2]);
         rmn.rtime = reminder_time;
 
+        // Check if DATA_DIRPATH exists, if not create it.
         if (mkdir(DATA_DIRPATH, 0755) == -1)
             if (errno != EEXIST)
             {
@@ -85,36 +91,16 @@ int main(int argc, char *argv[])
         FILE* mimi_data = fopen(DATA_FILEPATH, "a");
         if (!mimi_data) { printf("error: couldn't append to file %s\n", DATA_FILEPATH); return 1; }
 
+        // Write description and reminder date to DATA_FILEPATH.
         fprintf(mimi_data, DATA_FORMAT_OUT, rmn.desc, rmn.rtime);
         fclose(mimi_data);
     }
-    else if (!strcmp(argv[1], CMD_LIST0) || !strcmp(argv[1], CMD_LIST1))
-    {
-        if (argc != 2) { printf("error: invalid argument count\n"); return 1; }
-
-        FILE* mimi_data = fopen(DATA_FILEPATH, "r");
-        if (!mimi_data) { printf("error: couldn't read file %s\n", DATA_FILEPATH); return 1; }
-
-        size_t idx = 0;
-        char line_buf[128];
-        
-        while (fgets(line_buf, sizeof(line_buf), mimi_data))
-        {
-            printf("%zu\t", idx++);
-            char desc[64];
-            time_t rtime;
-            if (sscanf(line_buf, DATA_FORMAT_IN, desc, &rtime) == 2)
-            {
-                printf("%s\t%ld\n", desc, (long)rtime);
-            }
-        }
-
-        fclose(mimi_data);
-    }
+    // Delete reminders.
     else if (!strcmp(argv[1], CMD_DELETE0) || !strcmp(argv[1], CMD_DELETE1))
     {
         if (argc != 3) { printf("error: invalid argument count\n"); return 1; }
 
+        // Check if reminder index is a valid number.
         char *end;
         long argidx = strtol(argv[2], &end, 10);
         if (*end != '\0')
@@ -132,8 +118,9 @@ int main(int argc, char *argv[])
         reminder rmn;
 
         long idx = 0;
-        char line_buf0[128];
 
+        // Write everything except the reminder to delete into DATA_TEMP_FILEPATH.
+        char line_buf0[128];
         while (fgets(line_buf0, sizeof(line_buf0), mimi_data0))
         {
             if (argidx != idx &&
@@ -151,12 +138,35 @@ int main(int argc, char *argv[])
         FILE *mimi_temp_data1 = fopen(DATA_TEMP_FILEPATH, "r");
         if (!mimi_temp_data1) { printf("error: couldn't read file %s\n", DATA_TEMP_FILEPATH); return 1; }
 
+        // Write DATA_TEMP_FILEPATH contents to DATA_FILEPATH.
         char line_buf1[128];
         while (fgets(line_buf1, sizeof(line_buf1), mimi_temp_data1))
             fprintf(mimi_data1, "%s", line_buf1);
 
         fclose(mimi_data1);
         fclose(mimi_temp_data1);
+    }
+    // Verbosely list reminders.
+    else if (!strcmp(argv[1], CMD_LIST0) || !strcmp(argv[1], CMD_LIST1))
+    {
+        if (argc != 2) { printf("error: invalid argument count\n"); return 1; }
+
+        FILE* mimi_data = fopen(DATA_FILEPATH, "r");
+        if (!mimi_data) { printf("error: couldn't read file %s\n", DATA_FILEPATH); return 1; }
+
+        size_t idx = 0;
+        char line_buf[128];
+        
+        // Print reminders.
+        while (fgets(line_buf, sizeof(line_buf), mimi_data))
+        {
+            char desc[64];
+            time_t rtime;
+            if (sscanf(line_buf, DATA_FORMAT_IN, desc, &rtime) == 2)
+                printf("%zu\t%s\t%ld\n", idx++, desc, (long)rtime);
+        }
+
+        fclose(mimi_data);
     }
     else
     {
